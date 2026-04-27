962935174f1062d51983c7499cd866dccbefb448
Sayed Ali Abbas Rasool <abbas.rasool@metu.edu.tr>
2026-04-27 10:10:49 +0300

    Add LoadDirection enum for load classification

---
+++ FrameAnalysisProgram/STRUCTURAL_MODEL/Loads/LoadDirection.cs
@@ -0,0 +1,12 @@
+namespace FrameAnalysisProgram.STRUCTURAL_MODEL
+{
+    /// <summary>
+    /// Enumeration for load direction in global coordinates.
+    /// </summary>
+    public enum LoadDirection
+    {
+        X = 0,
+        Y = 1,
+        Z = 2
+    }
+}
